22e45eb8c2c5b990be3ef10155dfa587f48c2c7e
AlishaAng <bt16328@qmul.ac.uk>
2020-01-22 04:09:10 +0000

    added search page

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, Required
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.fields.html5 import EmailField
 from flask_wtf import Form
+from wtforms import SelectField
 
 # use this python module for creating python forms and user input 
 # import stringfields  so the user can input strings
@@ -11,7 +12,7 @@ from flask_wtf import Form
 
 
 
-class LoginForm(FlaskForm):		
+class Kinase(FlaskForm):		
 	search = StringField('Enter a valid Kinase name', validators=[DataRequired()])
 	submit = SubmitField('Search')
 
@@ -24,4 +25,11 @@ class Inhibitor(FlaskForm):
 class FileForm(FlaskForm):
     file = FileField(validators=[FileRequired(), FileAllowed(['csv', 'tsv'], "Please Upload only a tsv or csv")])
     submit = SubmitField('Submit')
+
+class Phosphosite(FlaskForm):
+	chromosome_number = SelectField('chromosome_number', choices=[])
+	kinase = SelectField('kinase', choices=[])
+
+#class ChoiceForm(FlaskForm):
+#    opts = QuerySelectField(query_factory=choice_query, allow_blank=False, get_label='name')
 
