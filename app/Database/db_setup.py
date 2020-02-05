bd35d63cda3d452e1cd480842a1a8f948c185df1
hannz88 <bt19623@qmul.ac.uk>
2020-02-05 18:21:39 +0000

    -updated the declarative and the importer and db

--- app/Database/db_setup.py
+++ app/Database/db_setup.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from .kinase_declarative import *
+from kinase_declarative import *
 
 #create engine and bine the engine
 engine = create_engine("sqlite:///kinase_database.db", connect_args={'check_same_thread': False}, convert_unicode=True)
