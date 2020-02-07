39b233eccf0d5bac0de1ae13b5115f3f7d73548e
MO105 <mohamed105@hotmail.co.uk>
2020-02-07 13:29:28 +0000

    added uniport id to col

--- app/Database/kinase_functions.py
+++ app/Database/kinase_functions.py
@@ -50,7 +50,7 @@ def get_gene_alias_protein_name(kinase_input):
     for meta in kinase_query:
         results = {}
         results["Gene_Name"] = meta.to_dict()["gene_name"]
-        results["Gene aliases"] = meta.to_dict()["gene_aliases"]
+        results["Gene_aliases"] = meta.to_dict()["gene_aliases"]
         results["Protein_Name"] = meta.to_dict()["protein_name"]
         results["Uniprot_id"] = meta.to_dict()["uniprot_number"]
         tmp.append(results)
