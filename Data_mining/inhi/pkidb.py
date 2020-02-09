5646a9417529f39078f697e9a7fc1d864e3bd4a1
AlishaAng <bt16328@qmul.ac.uk>
2020-02-09 02:35:33 +0000

    deleted:    Data_mining/Inhibitor/DATA_RETRIEVAL.ipyn 	deleted:    Data_mining/Inhibitor/Documentation.txt 	deleted:    Data_mining/Inhibitor/Final_inhibitors.csv 	deleted:    Data_mining/Inhibitor/Incomplete_inhibitor.csv 	deleted:    Data_mining/Inhibitor/PubChem_ncbi.csv 	deleted:    Data_mining/Inhibitor/Pubchem.csv 	deleted:    Data_mining/Inhibitor/README.txt 	deleted:    Data_mining/Inhibitor/desktop.ini 	deleted:    Data_mining/Inhibitor/gene_names.csv 	deleted:    Data_mining/Inhibitor/inhibitors_images.csv 	deleted:    Data_mining/Inhibitor/kinase_inhibitor_list.csv 	deleted:    Data_mining/Inhibitor/last_code_for_link.ipynb 	deleted:    Data_mining/Inhibitor/web_scrapping_for_image.py

--- Data_mining/inhi/pkidb.py
+++ Data_mining/inhi/pkidb.py
@@ -8,6 +8,8 @@ page = 'http://www.icoa.fr/pkidb/'
 #making a file name
 filename = "pkidb.csv"
 f = open(filename, "w")
+
+#adding header
 headers = "Inhibitor,Target,MW,Smiles, InChiKey,Synonyms,Images,ID\n"  #adding header to file
 f.write(headers)
 f.close()
@@ -25,7 +27,7 @@ page_soup = soup(page_html, 'html.parser')
 #grabbing content of just the body of the table
 table = page_soup.table.tbody
 
-#each inhibitor information separated by tr
+#finding each inhibitor by row (tr)
 all_tr = table.findAll("tr")
   
 
@@ -33,59 +35,73 @@ f = open(filename, "a+") #open file to append
 
 for each in all_tr:
 	try:
-		#extracts the name of the inhibitor
+		#extracting the name of the inhibitor
 		inhibitor = each.td.b
 		inhibitor = inhibitor.text
 
+		#each row has value in columns made up by td
 		TD = each.findAll("td")
+
+		#extracting the target
 		Target1 = str(TD[16]).replace("<br/>", ",")
 		Target2 = Target1.strip("<td></")
 
+		#extracting the molecular weight
 		MW = TD[9].text
 
+		#extracting smiles formula
 		smiles = each.find("td", {"style":"word-wrap: break-word; max-width: 250px;"})
-		s = smiles.text
-		start = 'Smiles=;'
-		end = 'InChiKey'
+		s = smiles.text        #smiles and inchikey are found together in one text
+		start = 'Smiles=;'     #extracting text after "Smiles="
+		end = 'InChiKey'       # and before "InChiKey"
 		SMILES= s[s.find(start)+len(start):s.rfind(end)]
 
+		#extracting InchiKey
 		KEY = s.split('InChiKey=')[-1]
 
-		synonyms= str(TD[-3].text).replace("  ", ",")
+		#extracting synonyms
+		synonyms= TD[-3].get_text(strip=True, separator=',')
 
+		#extracting image link 
 		image = each.td
-		image1 = str(image.img["src"])
-		URL= "http://www.icoa.fr/pkidb/"
+		image1 = str(image.img["src"])  #src only gives the end of the link e.g 'static/img/mol/Leniolisib.svg'
+		URL= "http://www.icoa.fr/pkidb/" #this is the start of the link
 
-		IMAGE = URL + image1
+		IMAGE = URL + image1   #together we get 
 
 		def chemblID(each):
-		    ChemblID0 = each.findAll("td")[4].findAll("a")[0]
-		    ChemblID1 = each.findAll("td")[4].findAll("a")[1]
-		    ChemblID2 = each.findAll("td")[4].findAll("a")[2]
-		    
-		    ChemblID_link0 = ChemblID0.get("href")
-		    ChemblID_link1 = ChemblID1.get("href")
-		    ChemblID_link2 = ChemblID2.get("href")
-		    
-		    must_have = "CHEMBL"
+			ChemblID0 = each.findAll("td")[4].findAll("a")[0]
+			ChemblID1 = each.findAll("td")[4].findAll("a")[1]
+			ChemblID2 = each.findAll("td")[4].findAll("a")[2]
 
-		    if must_have in ChemblID_link0:
-		        ID = ChemblID_link0.split('inspect/')[-1]
-		        return ID
-		    if must_have in ChemblID_link1:
-		        ID = ChemblID_link1.split('inspect/')[-1]
-		        return ID
-		    if must_have in ChemblID_link2:
-		        ID = ChemblID_link2.split('inspect/')[-1]
-		        return ID 
+			ChemblID_link0 = ChemblID0.get("href")
+			ChemblID_link1 = ChemblID1.get("href")
+			ChemblID_link2 = ChemblID2.get("href")
+
+			must_have = "CHEMBL"
+
+			if must_have in ChemblID_link0:
+			    ID = ChemblID_link0.split('inspect/')[-1]
+			    return ID
+			else:
+			    pass
+			if must_have in ChemblID_link1:
+			    ID = ChemblID_link1.split('inspect/')[-1]
+			    return ID
+			else:
+			    pass
+			if must_have in ChemblID_link2:
+			    ID = ChemblID_link2.split('inspect/')[-1]
+			    return ID  
+			else:
+			    pass
 
 		ID = chemblID(each)
 		#adding the results to the file
 		f.write('"{}","{}","{}","{}","{}","{}", "{}", "{}"'.format(inhibitor, Target2, MW, SMILES, KEY, synonyms, IMAGE, ID )+ "\n")
 	
 	except:
-		pass
+		continue
 
 f.close()
 
